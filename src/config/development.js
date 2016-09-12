e1517f9379b0496eaf81f847d2e3bff0d2d66cab
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-09-11 21:23:50 -0500

    fix env variable

--- src/config/development.js
+++ src/config/development.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiUrl: process.env.API_URL || "http://localhost:8080",
+  apiUrl: process.env.REACT_APP_API_URL || "http://localhost:8080",
   logging: process.env.REACT_APP_LOGGING || true
 };
