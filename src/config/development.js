a0b121ec98a74c468e210f8d63c9fafa2fbc6766
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-09-11 20:55:51 -0500

    adding env congif

--- src/config/development.js
+++ src/config/development.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiUrl: "http://localhost:8080",
+  apiUrl: process.env.API_URL || "http://localhost:8080",
   logging: process.env.REACT_APP_LOGGING || true
 };
--- src/config/testing.js
+++ src/config/testing.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiUrl: "http://localhost:8080",
+  apiUrl: process.env.API_URL,
   logging: process.env.REACT_APP_LOGGING || true,
 };
